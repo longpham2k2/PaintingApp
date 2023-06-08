045a2132a2bb926d1e2cbdbf9009e0f8023a0e70
longpham2k2 <longpham2k2@gmail.com>
2023-06-08 11:32:21 +0700

    Change url

--- painting_app/urls.py
+++ painting_app/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('painting/', include('painting.urls')),
+    path('', include('painting.urls')),
 
 ]
 
