197bce68ee2315592672030a9d99bbd510b2d6cb
longpham2k2 <longpham2k2@gmail.com>
2023-06-17 00:14:14 +0700

    Full CRUD for painting, add routes for auth

--- painting_app/settings.py
+++ painting_app/settings.py
@@ -131,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIRR,'media')
+
+LOGIN_URL = '/admin/login/'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
